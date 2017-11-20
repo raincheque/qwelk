cae20741cba05c649493346008c5e5a9f0aa695a
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-11-20 09:38:33 +0330

    child creation example code so I can remember

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -3,6 +3,7 @@
 
 struct ModuleAutomaton : Module {
     enum ParamIds {
+        PARAM_LIGHT,
         NUM_PARAMS
     };
     enum InputIds {
@@ -12,14 +13,21 @@ struct ModuleAutomaton : Module {
         NUM_OUTPUTS
     };
     enum LightIds {
+        LIGHT_MUTE,
         NUM_LIGHTS
     };
 
+    int state = 0;
+    SchmittTrigger trig;
+    
     ModuleAutomaton() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
     void step override;
 }
 
 void ModuleAutomaton::step() {
+    if (trig.process(params[PARAM_LIGHT].value))
+        state ^= 1;
+    lights[LIGHT_MUTE].setBrightness(state ? 0.9 : 0.0);
 }
 
 template <typename _BASE>
@@ -37,6 +45,20 @@ WidgetAutomaton::WidgetAutomaton() {
 
     setPanel(SVG::load(assetPlugin(plugin, "res/Automaton.svg")));
 
-    //add children
+    addChild(createScrew<ScrewSilver>(Vec(15, 0)));
+    addChild(createScrew<ScrewSilver>(Vec(box.size.x - 30, 0)));
+    addChild(createScrew<ScrewSilver>(Vec(15, 365)));
+    addChild(createScrew<ScrewSilver>(Vec(box.size.x - 30, 365)));
+    
+        addParam(createParam<Davies1900hBlackKnob>(Vec(28, 87), module, MyModule::PITCH_PARAM, -3.0, 3.0, 0.0));
+
+    addInput(createInput<PJ301MPort>(Vec(33, 186), module, MyModule::PITCH_INPUT));
+
+    addOutput(createOutput<PJ301MPort>(Vec(33, 275), module, MyModule::SINE_OUTPUT));
+
+    addChild(createLight<MediumLight<RedLight>>(Vec(41, 59), module, MyModule::BLINK_LIGHT));
+
+    addParam(createParam<LEDBezel>(Vec(50, 60), module, PARAM_LIGHT, 0.0, 1.0, 0.0));
+    addChild(createLight<MuteLight<GreenLight>>(Vec(50, 60), module, LIGHT_MUTE));
 }
 
