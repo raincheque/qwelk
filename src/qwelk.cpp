b44f7f69c89538e44f6d74c61207698c8bffefe8
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-11-21 10:39:50 +0330

    widget helpers for BitCounter

--- src/qwelk.cpp
+++ src/qwelk.cpp
@@ -14,6 +14,7 @@ void init(rack::Plugin *p) {
 
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
 	p->addModel(createModel<WidgetAutomaton>("Qwelk", "Automaton", "Automaton", SEQUENCER_TAG));
+    p->addModel(createModel<WidgetBitCounter>("Qwelk", "BitCounter", "Bit Counter", SEQUENCER_TAG));
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
--- src/qwelk.hpp
+++ src/qwelk.hpp
@@ -7,3 +7,7 @@ extern Plugin *plugin;
 struct WidgetAutomaton : ModuleWidget {
     WidgetAutomaton();
 };
+
+struct WidgetBitCounter : ModuleWidget {
+    WidgetBitCounter();
+};
