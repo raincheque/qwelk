9597c9b8726c3027208e4487ea2c727508fb65f6
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-11-22 16:25:31 +0330

    save, load, reset and randomize implemented / fixed SCAN input bug

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -1,4 +1,5 @@
 #include "dsp/digital.hpp"
+#include "math.hpp"
 #include "qwelk.hpp"
 
 
@@ -33,6 +34,7 @@ struct ModuleAutomaton : Module {
     };
 
     int             scan = 1;
+    int             scan_sign = 0;
     SchmittTrigger  trig_step_input;
     SchmittTrigger  trig_step_manual;
     SchmittTrigger  trig_scan_manual;
@@ -45,7 +47,51 @@ struct ModuleAutomaton : Module {
 
     
     ModuleAutomaton() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+
     void step() override;
+
+    json_t *toJson() override {
+		json_t *rootJ = json_object();
+
+		json_object_set_new(rootJ, "scan", json_integer(scan));
+
+		json_t *statesJ = json_array();
+		for (int i = 0; i < CHANNELS*2; i++) {
+			json_t *stateJ = json_integer(states[i]);
+			json_array_append_new(statesJ, stateJ);
+		}
+		json_object_set_new(rootJ, "states", statesJ);
+
+		return rootJ;
+	}
+
+	void fromJson(json_t *rootJ) override {
+		json_t *scanJ = json_object_get(rootJ, "scan");
+		if (scanJ)
+			scan = json_integer_value(scanJ);
+
+		// gates
+		json_t *statesJ = json_object_get(rootJ, "states");
+		if (statesJ) {
+			for (int i = 0; i < 8; i++) {
+				json_t *gateJ = json_array_get(statesJ, i);
+				if (gateJ)
+					states[i] = json_integer_value(gateJ);
+			}
+		}
+	}
+
+	void reset() override {
+        scan = 1;
+		for (int i = 0; i < CHANNELS * 2; i++)
+			states[i] = 0;
+	}
+
+	void randomize() override {
+        scan = (randomf() > 0.5) ? 1 : -1;
+		for (int i = 0; i < CHANNELS; i++)
+			states[i] = (randomf() > 0.5);
+	}
 };
 
 void ModuleAutomaton::step()
@@ -56,8 +102,9 @@ void ModuleAutomaton::step()
         nextstep = 1;
 
     // determine scan direction
-    if (trig_scan_input.process(inputs[INPUT_SCAN].value))
-        scan = inputs[INPUT_SCAN].normalize(scan) < 0 ? -1 : 1;
+    int scan_input_sign = (int)sgnf(inputs[INPUT_SCAN].normalize(scan));
+    if (scan_input_sign != scan_sign) 
+        scan = scan_sign = scan_input_sign;
     // manual tinkering with step?
     if (trig_scan_manual.process(params[PARAM_SCAN].value))
         scan *= -1;
