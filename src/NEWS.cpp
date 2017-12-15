609094f3fc82dfcc166dda7ddc10afc8ebda2472
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-12-15 22:48:48 +0330

    intensity, wrap & hold inputs

--- src/NEWS.cpp
+++ src/NEWS.cpp
@@ -22,6 +22,9 @@ struct ModuleNews : Module {
     };
     enum InputIds {
         IN_NEWS,
+        IN_INTENSITY,
+        IN_WRAP,
+        IN_HOLD,
         NUM_INPUTS
     };
     enum OutputIds {
@@ -33,7 +36,9 @@ struct ModuleNews : Module {
         NUM_LIGHTS = LIGHT_GRID + GWIDTH * GHEIGHT
     };
 
-    byte grid[GWIDTH * GHEIGHT] {};    
+    float sample;
+    SchmittTrigger trig_hold;
+    byte grid[GWIDTH * GHEIGHT] {};
 
     ModuleNews() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
     void step() override;
@@ -47,15 +52,25 @@ void ModuleNews::step()
     bool    clamp       = params[PARAM_CLAMP].value == 0.0;
     byte    intensity   = (byte)(floor(params[PARAM_INTENSITY].value));
     byte    wrap        = (byte)(floor(params[PARAM_WRAP].value));
-    float   news        = inputs[IN_NEWS].value;
 
+    float in_intensity  = (inputs[IN_INTENSITY].value / 10.0) * 255.0;
+    float in_wrap       = (inputs[IN_WRAP].value / 10.0) * 31.0;
+
+    intensity = (intensity + (byte)in_intensity > 255.0) ? 255.0 : intensity + (byte)in_intensity;
+    wrap = (wrap + (byte)in_wrap > 31.0) ? 31.0 : wrap + (byte)in_wrap;
+
+    if (trig_hold.process(inputs[IN_HOLD].value))
+        sample = inputs[IN_NEWS].value;
+    
+    float news = (inputs[IN_HOLD].active) ?  sample : inputs[IN_NEWS].value;
+
+    if (round)
+        news = (int)news;
+        
     unsigned bits = *(reinterpret_cast<unsigned *>(&news));
     bits = (bits << wrap) | (bits >> (32 - wrap));
 
     news = *((float *)&bits);
-    
-    if (round)
-        news = (int)news;
 
     unsigned key = *(reinterpret_cast<unsigned *>(&news));
 
@@ -161,13 +176,19 @@ WidgetNews::WidgetNews()
     addChild(createScrew<ScrewSilver>(Vec(15, 365)));
     addChild(createScrew<ScrewSilver>(Vec(box.size.x - 30, 365)));
 
-    addParam(createParam<TinyKnob>(Vec(10   , 30), module, ModuleNews::PARAM_INTENSITY, 1.0, 255.0, 4.0));
-    addParam(createParam<TinyKnob>(Vec(40   , 30), module, ModuleNews::PARAM_WRAP, 1.0, 31.0, 0.0));
-    addParam(createParam<CKSS>(Vec(40       , 60), module, ModuleNews::PARAM_MODE, 0.0, 1.0, 1.0));
-    addParam(createParam<CKSS>(Vec(60       , 60), module, ModuleNews::PARAM_GATEMODE, 0.0, 1.0, 1.0));
-    addParam(createParam<CKSS>(Vec(80       , 60), module, ModuleNews::PARAM_ROUND, 0.0, 1.0, 1.0));
-    addParam(createParam<CKSS>(Vec(100      , 60), module, ModuleNews::PARAM_CLAMP, 0.0, 1.0, 1.0));
-    addInput(createInput<PJ301MPort>(Vec(10, 60), module, ModuleNews::IN_NEWS));
+
+    addInput(createInput<PJ301MPort>(Vec(10 , 30), module, ModuleNews::IN_NEWS));
+    addInput(createInput<PJ301MPort>(Vec(40 , 30), module, ModuleNews::IN_HOLD));
+    addInput(createInput<PJ301MPort>(Vec(70 , 30), module, ModuleNews::IN_INTENSITY));
+    addInput(createInput<PJ301MPort>(Vec(100, 30), module, ModuleNews::IN_WRAP));
+    addParam(createParam<CKSS>(Vec(5        , 60), module, ModuleNews::PARAM_MODE, 0.0, 1.0, 1.0));
+    addParam(createParam<CKSS>(Vec(23       , 60), module, ModuleNews::PARAM_GATEMODE, 0.0, 1.0, 1.0));
+    addParam(createParam<CKSS>(Vec(41       , 60), module, ModuleNews::PARAM_ROUND, 0.0, 1.0, 1.0));
+    addParam(createParam<CKSS>(Vec(59       , 60), module, ModuleNews::PARAM_CLAMP, 0.0, 1.0, 1.0));
+    addParam(createParam<TinyKnob>(Vec(80   , 60), module, ModuleNews::PARAM_INTENSITY, 1.0, 255.0, 4.0));
+    addParam(createParam<TinyKnob>(Vec(105  , 60), module, ModuleNews::PARAM_WRAP, 1.0, 31.0, 0.0));
+
+
     
     for (int y = 0; y < GHEIGHT; ++y)
         for (int x = 0; x < GWIDTH; ++x) {
