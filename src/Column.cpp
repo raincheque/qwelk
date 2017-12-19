41f43781d9f55b3453a550f8b76246670080c087
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-12-19 12:25:27 +0330

    testing an avg algo

--- src/Column.cpp
+++ src/Column.cpp
@@ -8,6 +8,7 @@
 struct ModuleColumn : Module {
     enum ParamIds {
         PARAM_MASTER,
+        PARAM_AVG,
         NUM_PARAMS
     };
     enum InputIds {
@@ -29,14 +30,26 @@ struct ModuleColumn : Module {
 };
 
 void ModuleColumn::step() {
+    bool avg = params[PARAM_AVG].value == 0.0;
+
+    int onno = 0;
     float dsum = 0.0;
     for (int i = 0; i < CHANNELS; ++i) {
         float usv = inputs[IN_UPSTREAM + i].value;
         float val = inputs[IN_SIG + i].value;
-        
+
+        if (inputs[IN_UPSTREAM + i].active && usv != 0.0)
+            onno++;
+        if (inputs[IN_SIG + i].active && val != 0.0)
+            onno++;
+
         outputs[OUT_SIDE + i].value = val;
+
+        float sum = usv + val;
+        dsum += sum;
         
-        dsum += usv + val;
+        if (avg && onno > 0)
+            dsum = dsum / onno;
         
         outputs[OUT_DOWNSTREAM + i].value = dsum;
     }
@@ -47,7 +60,7 @@ WidgetColumn::WidgetColumn() {
     ModuleColumn *module = new ModuleColumn();
     setModule(module);
 
-    box.size = Vec(4 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+    box.size = Vec(6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
     {
         SVGPanel *panel = new SVGPanel();
         panel->box.size = box.size;
@@ -58,10 +71,12 @@ WidgetColumn::WidgetColumn() {
     addChild(createScrew<ScrewSilver>(Vec(15, 0)));
     addChild(createScrew<ScrewSilver>(Vec(15, 365)));
 
-    float x = 2.5, xstep = 15, ystep = 27;
+    addParam(createParam<CKSS>(Vec(3, 30), module, ModuleColumn::PARAM_AVG, 0.0, 1.0, 1.0));
+
+    float x = 7.5, xstep = 25, ystep = 20;
     for (int i = 0; i < CHANNELS; ++i)
     {
-        float y = 40 + i * 90;
+        float y = 90 + i * 70;
         addInput(createInput<PJ301MPort>(Vec(x + xstep, y - ystep), module, ModuleColumn::IN_UPSTREAM + i));
         addOutput(createOutput<PJ301MPort>(Vec(x + xstep*2, y), module, ModuleColumn::OUT_SIDE + i));
         addInput(createInput<PJ301MPort>(Vec(x, y), module, ModuleColumn::IN_SIG + i));
