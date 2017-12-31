f083ae44cec817b4d9eaba99beb994a966c2a1c0
Parsa Jamshidi <parsa.jamshidi.0@gmail.com>
2017-12-31 13:43:21 +0330

    gain control for LR section / viz fixup

--- src/Mix.cpp
+++ src/Mix.cpp
@@ -6,6 +6,9 @@ struct ModuleMix : Module {
         PARAM_GAIN_M,
         PARAM_GAIN_S,
         PARAM_GAIN_MS,
+        PARAM_GAIN_L,
+        PARAM_GAIN_R,
+        PARAM_GAIN_LR,
         NUM_PARAMS
     };
     enum InputIds {
@@ -15,6 +18,8 @@ struct ModuleMix : Module {
         IN_S,
         IN_GAIN_M,
         IN_GAIN_S,
+        IN_GAIN_L,
+        IN_GAIN_R,
         NUM_INPUTS
     };
     enum OutputIds {
@@ -31,11 +36,11 @@ struct ModuleMix : Module {
     ModuleMix() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
     void step() override;
 };
-
+static inline float max(float a, float b) {return a < b ? b : a;}
 void ModuleMix::step() {
     if (inputs[IN_L].active && inputs[IN_R].active) {
-        float iam = inputs[IN_GAIN_M].value / 10.0;
-        float ias = inputs[IN_GAIN_S].value / 10.0;
+        float iam = max(inputs[IN_GAIN_M].value, 0) / 10.0;
+        float ias = max(inputs[IN_GAIN_S].value, 0) / 10.0;
         float ams = params[PARAM_GAIN_MS].value;
         float am = inputs[IN_GAIN_M].active ? params[PARAM_GAIN_M].value * iam : params[PARAM_GAIN_M].value;
         float as = inputs[IN_GAIN_S].active ? params[PARAM_GAIN_S].value * ias : params[PARAM_GAIN_S].value;
@@ -47,12 +52,17 @@ void ModuleMix::step() {
         outputs[OUT_S].value = s * ams * as;
     }
     if (inputs[IN_M].active && inputs[IN_S].active) {
+        float ial = max(inputs[IN_GAIN_L].value, 0) / 10.0;
+        float iar = max(inputs[IN_GAIN_R].value, 0) / 10.0;
+        float alr = params[PARAM_GAIN_LR].value;
+        float al = inputs[IN_GAIN_L].active ? params[PARAM_GAIN_L].value * ial : params[PARAM_GAIN_L].value;
+        float ar = inputs[IN_GAIN_R].active ? params[PARAM_GAIN_R].value * iar : params[PARAM_GAIN_R].value;
         float m = inputs[IN_M].value;
         float s = inputs[IN_S].value;
         float l = (m + s) * 0.5;
         float r = (m - s) * 0.5;
-        outputs[OUT_L].value = l;
-        outputs[OUT_R].value = r;
+        outputs[OUT_L].value = l * alr * al;
+        outputs[OUT_R].value = r * alr * ar;
     }
 }
 
@@ -71,27 +81,38 @@ WidgetMix::WidgetMix() {
     {
         SVGPanel *panel = new SVGPanel();
         panel->box.size = box.size;
-        panel->setBackground(SVG::load(assetPlugin(plugin, "res/Blank_8.svg")));
+        panel->setBackground(SVG::load(assetPlugin(plugin, "res/Mix.svg")));
         addChild(panel);
     }
 
-    addChild(createScrew<ScrewSilver>(Vec(15, 0)));
-    addChild(createScrew<ScrewSilver>(Vec(15, 365)));
+    addChild(createScrew<ScrewSilver>(Vec(5, 0)));
+    addChild(createScrew<ScrewSilver>(Vec(5, 365)));
 
-    float x = box.size.x / 2.0 - 30;
+    float x = box.size.x / 2.0 - 27;
     
-    addInput(createInput<PJ301MPort>(Vec(x  ,   30), module, ModuleMix::IN_L));
-    addInput(createInput<PJ301MPort>(Vec(x  ,   60), module, ModuleMix::IN_R));
-    addParam(createParam<RoundTinyKnob>(Vec(x,  90), module, ModuleMix::PARAM_GAIN_MS, 0.0, 1.0, 1.0));
-    addOutput(createOutput<PJ301MPort>(Vec(x,  120), module, ModuleMix::OUT_M));
-    addParam(createParam<RoundTinyKnob>(Vec(x, 150), module, ModuleMix::PARAM_GAIN_M, 0.0, 1.0, 1.0));
-    addInput(createInput<PJ301MPort>(Vec(x + 25 ,  150), module, ModuleMix::IN_GAIN_M));
-    addOutput(createOutput<PJ301MPort>(Vec(x,  180), module, ModuleMix::OUT_S));
-    addParam(createParam<RoundTinyKnob>(Vec(x, 210), module, ModuleMix::PARAM_GAIN_S, 0.0, 1.0, 1.0));
-    addInput(createInput<PJ301MPort>(Vec(x + 25 ,  210), module, ModuleMix::IN_GAIN_S));
+    addInput(createInput<PJ301MPort>(Vec(x     ,   25), module, ModuleMix::IN_L));
+    addInput(createInput<PJ301MPort>(Vec(x + 30,   25), module, ModuleMix::IN_R));
+    
+    addParam(createParam<RoundSmallBlackKnob>(Vec(x + 28,  55), module, ModuleMix::PARAM_GAIN_MS, 0.0, 1.0, 1.0));
 
-    addInput(createInput<PJ301MPort>(Vec(x  , 240), module, ModuleMix::IN_M));
-    addInput(createInput<PJ301MPort>(Vec(x  , 270), module, ModuleMix::IN_S));
-    addOutput(createOutput<PJ301MPort>(Vec(x, 300), module, ModuleMix::OUT_L));
-    addOutput(createOutput<PJ301MPort>(Vec(x, 330), module, ModuleMix::OUT_R));
+    addParam(createParam<RoundTinyKnob>(Vec(x    , 90), module, ModuleMix::PARAM_GAIN_M, 0.0, 1.0, 1.0));
+    addInput(createInput<PJ301MPort>(Vec(x + 30  , 88), module, ModuleMix::IN_GAIN_M));
+    addOutput(createOutput<PJ301MPort>(Vec(x + 30, 113), module, ModuleMix::OUT_M));
+
+    addParam(createParam<RoundTinyKnob>(Vec(x    , 147), module, ModuleMix::PARAM_GAIN_S, 0.0, 1.0, 1.0));
+    addInput(createInput<PJ301MPort>(Vec(x + 30  , 145), module, ModuleMix::IN_GAIN_S));
+    addOutput(createOutput<PJ301MPort>(Vec(x + 30, 169), module, ModuleMix::OUT_S));
+
+    addInput(createInput<PJ301MPort>(Vec(x     , 210), module, ModuleMix::IN_M));
+    addInput(createInput<PJ301MPort>(Vec(x + 30, 210), module, ModuleMix::IN_S));
+
+    addParam(createParam<RoundSmallBlackKnob>(Vec(x + 28,  240), module, ModuleMix::PARAM_GAIN_LR, 0.0, 1.0, 1.0));
+
+    addParam(createParam<RoundTinyKnob>(Vec(x    , 275), module, ModuleMix::PARAM_GAIN_L, 0.0, 1.0, 1.0));
+    addInput(createInput<PJ301MPort>(Vec(x + 30  , 273), module, ModuleMix::IN_GAIN_L));
+    addOutput(createOutput<PJ301MPort>(Vec(x + 30, 298), module, ModuleMix::OUT_L));
+
+    addParam(createParam<RoundTinyKnob>(Vec(x    , 332), module, ModuleMix::PARAM_GAIN_R, 0.0, 1.0, 1.0));
+    addInput(createInput<PJ301MPort>(Vec(x + 30  , 330), module, ModuleMix::IN_GAIN_R));
+    addOutput(createOutput<PJ301MPort>(Vec(x + 30, 355), module, ModuleMix::OUT_R));
 }
